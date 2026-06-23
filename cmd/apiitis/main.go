41b9cf2397bab1e0d5011c70be34b5e985e02d9b
@12345Asmit <asmitshukla9794@gmail.com>
2026-06-23 12:42:53 +0530

    feat : function created for storing data into database coming from the backend

--- cmd/apiitis/main.go
+++ cmd/apiitis/main.go
@@ -22,7 +22,7 @@ func main() {
 
 	//setup datbase
 
-	_, err := sqlite.New(cfg)
+	storage, err := sqlite.New(cfg)
 
 	if err != nil {
 		log.Fatalf("failed to setup database %s", err.Error())
@@ -31,7 +31,7 @@ func main() {
 	//setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("POST /api/students",student.New())
+	router.HandleFunc("POST /api/students",student.New(storage))
 	//setup server
 	fmt.Println(" server started")
 	
--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -8,13 +8,14 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/Asmitshukl/apiitis/internal/storage"
 	"github.com/Asmitshukl/apiitis/internal/types"
 	"github.com/Asmitshukl/apiitis/internal/utils/response"
 	"github.com/go-playground/validator/v10"
 )
 
 
-func New() http.HandlerFunc{
+func New(storage storage.Storage) http.HandlerFunc{
 	return func(w http.ResponseWriter , r *http.Request) {
 
 		var student types.Student
@@ -41,6 +42,18 @@ func New() http.HandlerFunc{
 			return
 		}
 
-		response.WriteJson(w , http.StatusCreated , student)
+		id , err :=storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+
+		if err != nil {
+			response.WriteJson(w , http.StatusInternalServerError , err)
+			return
+		}
+		slog.Info("User created successfully",slog.String("userid",fmt.Sprint(id)))
+
+		response.WriteJson(w , http.StatusCreated , map[string]int64{"id": id})
 	}
 }
